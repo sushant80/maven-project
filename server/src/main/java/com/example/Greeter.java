462ccf87a08cd7b9584f9eb5832253d9f282bdf8
Sushant Panigrahi <sushant.panigrahi@infostretch.com>
2019-04-20 11:07:44 -0500

    fix checkstyle warning

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,10 +11,15 @@ public class Greeter {
   public Greeter() {
 
   }
-/*
-@param someone the name of boss
-@return greeting string
-*/
+/**
+ * Extracts the user's name from the input arguments.
+ *
+ * Precondition: 'args' should contain at least one element, the user's name.
+ *
+ * @param  args            the command-line arguments.
+ * @return                 the user's name (the first command-line argument).
+ * @throws NoNameException if 'args' contains no element.
+ */
 /* this is for comment to avoid warning */
   public final  String greet(final String someone) {
     return String.format("Hello, %s!", someone);
